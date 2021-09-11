a46bbc707297683a1184d65312ea4dc935847390
Haris123Android <82931832+Haris123Android@users.noreply.github.com>
2021-09-11 18:21:49 +0500

    Add files via upload

---
+++ spliceArray.js
@@ -0,0 +1,4 @@
+let animalss = ['Shark', 'Salmon', 'Whale', 'Bear', 'Lizard','Tortoise'];
+// for insereting or deleting an element in the meddle of an array we use splice
+animalss.splice(1,0,'Star');
+console.log(animalss);
